1ba93d7f7cfb5c8929afc8f764f1851ada41d315
Gabriel Isaac <stewartisaac90@gmail.com>
2024-07-17 22:30:04 +0100

    update form logic

--- script.js
+++ script.js
@@ -1,56 +1,51 @@
-/*  Adding DOMContentLoaded Event listener ensures JavaScript runs 
-    after the entire HTML document has been loaded */
 document.addEventListener('DOMContentLoaded', () => {
-    // Form selection
-    const form = document.getElementById('registration-form');
-    form.addEventListener('submit', (event) =>{
-        event.preventDefault();
-    });
+    (function() {
+        // Form selection
+        const form = document.getElementById('registration-form');
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
 
-    // Retrieve user inputs and trim
-    const usernameInput = document.getElementById('username');
-    const username = usernameInput.value.trim();
-    
+            // Retrieve user inputs and trim
+            const usernameInput = document.getElementById('username');
+            const username = usernameInput.value.trim();
 
-    const emailInput = document.getElementById('email');
-    const email = emailInput.value.trim();
-    
+            const emailInput = document.getElementById('email');
+            const email = emailInput.value.trim();
 
-    const passwordInput = document.getElementById('password');
-    const password = passwordInput.value.trim();
+            const passwordInput = document.getElementById('password');
+            const password = passwordInput.value.trim();
 
-    // Initialize validation variables
-    let isValid = true;
-    let messages = [];
+            // Initialize validation variables
+            let isValid = true;
+            let messages = [];
 
-    if (username.length < 3){      // username validation
-        isValid = false;
-        messages.push("Your username is less than 3 characters");
-    };
+            if (username.length < 3) { // username validation
+                isValid = false;
+                messages.push("Your username is less than 3 characters");
+            }
 
-    if (email.includes('@') || email.includes('.')) {       // email validation
-        isValid = false;
-        messages.push("Enter a valid email");
-    };
+            if (!email.includes('@') || !email.includes('.')) { // email validation
+                isValid = false;
+                messages.push("Enter a valid email");
+            }
 
-    if (password.length < 8) {     // password validation
-        isValid = false;
-        messages.push("Password must be 8 characters or more");
-    };
+            if (password.length < 8) { // password validation
+                isValid = false;
+                messages.push("Password must be 8 characters or more");
+            }
 
+            // Feedback div selection
+            const feedbackDiv = document.getElementById('form-feedback');
 
-    // Feedback div selection
-    const feedbackDiv = document.getElementById('form-feedback');
-
-    // feedback display logic
-    feedbackDiv.style.display = "block";
-
-    if (isValid) {
-        feedbackDiv.textContent = "Registration is successful!";
-        feedbackDiv.style.color = "#28a745";
-    } else {
-        feedbackDiv.innerHTML = messages.join("<br>");
-        feedbackDiv.style.color = "#dc3545";
-    };
-
+            // feedback display logic
+            feedbackDiv.style.display = "block";
+            if (isValid) {
+                feedbackDiv.textContent = "Registration is successful!";
+                feedbackDiv.style.color = "#28a745";
+            } else {
+                feedbackDiv.innerHTML = messages.join("<br>");
+                feedbackDiv.style.color = "#dc3545";
+            }
+        });
+    })();
 });
